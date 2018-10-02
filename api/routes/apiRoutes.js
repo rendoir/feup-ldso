b3fd8274d115dbe6224c00927f6634d90463e935
Daniel Marques <up201503822@fe.up.pt>
2018-10-02 16:16:40 +0100

    Started working on th event controller

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -2,11 +2,16 @@ var express = require("express");
 var middleware = require("../middleware");
 var router = express.Router();
 
-// Add event
-router.post("/", function(req, res) {
-    
+var event_controller = require("../controllers/EventController");
+
+router.get("/", function(req, res) {
+
 });
   
-
+router.post("/addEvent", function(req, res) {
+    if(event_controller.isValidEvent(req.body))
+        event_controller.addEvent(req.body);
+    res.send();
+});
 
 module.exports = router;
