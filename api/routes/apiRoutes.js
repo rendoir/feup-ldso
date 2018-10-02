8f8f547a1165ed675d2b7ad17d71dd67b975f64c
beemargarida <up201505505@gcloud.fe.up.pt>
2018-10-02 12:20:03 +0100

    Route to add event created. Still doens't do anything

---
+++ api/routes/apiRoutes.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var middleware = require("../middleware");
+var router = express.Router();
+
+// Add event
+router.post("/", function(req, res) {
+    
+});
+  
+
+
+module.exports = router;
