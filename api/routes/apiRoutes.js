d27c865beaa88832c4810da50aac97bb598d6263
Vitor Magalhaes <up201503447@fe.up.pt>
2018-10-03 20:08:44 +0100

    Needs testing

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -1,42 +1,56 @@
+var { Client } = require('pg');
+
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:example@localhost:5432/postgres';
+
+const client = new Client(connectionString);
+
 var express = require("express");
 var middleware = require("../middleware");
 var router = express.Router();
 
-var NUMBER_EVENTS = 10;
-
-const {Client} = require("pg");
-const client = new Client();
-
 client.connect();
 
+var NUMBER_EVENTS = 10;
+
 // Add event
-router.post("/", function(req, res) {
+router.post("/", function (req, res) {
 
 });
 
 // List all events
-router.get("/events/:page", function(req, res) {
 
-    if (err){
-        console.log("error showing events");
-    }
-    else{
-        let id = req.param.version;
-        let pages = id * NUMBER_EVENTS;
-        let today = Date.now();
-        const query = "SELECT * FROM event WHERE event.start >= " + today + "ORDER BY event.start ASC OFFSET " 
-                      + pages-NUMBER_EVENTS + "LIMIT " + NUMBER_EVENTS;
+router.get("/events/:page", function(req,res) {
 
-        client.query(text, (err, res) => {
+     //SELECT * FROM events WHERE events.start_date >= '2018-10-01' ORDER BY events.start_date OFFSET 0 LIMIT 10;
+    let id = req.params.page;
+    let pages = id * 10;
+    let today = new Date().toISOString().slice(0,10);
 
-            if (err) {
-                console.log(err.stack);
-            } else {
-                console.log(res.rows[0]);
-            }
-        })
+    const query = "SELECT * FROM events WHERE events.start_date >= ? ORDER BY events.start_date OFFSET ? LIMIT ?";
+    const values = [today, pages - NUMBER_EVENTS, NUMBER_EVENTS];
+
+    client.connect();
+
+    client.query(query, values, (err,results) => {
         
-    }
+        if(err){
+            console.log("Error");
+            res.statusCode = 500;
+            return res.json({errors: ["No Events found"]});
+        }
+        
+        if(results.rows.length === 0){
+            res.statusCode = 404;
+            return res.json({ errors: ["No Events found"]})
+        }
+
+        req.events = results.rows;
+        next();
+
+    });
+
+    client.end();
+
 });
 
 
