93d8a768d7b793eaceb60ea2bea57f388c981934
beemargarida <up201505505@fe.up.pt>
2018-10-12 21:28:41 +0100

    Fix in test. Now it ends after running all tests

--- api/config/config.js
+++ api/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     development: {
         dialect: "postgres",
         user: "postgres",
-        host: "192.168.99.100", //192.168.99.100 for Vitor; localhost for everyone else
+        host: "localhost", //192.168.99.100 for Vitor; localhost for everyone else
         database: "postgres",
         password: "example",
         port: "5432",
--- api/test/event.js
+++ api/test/event.js
@@ -41,89 +41,53 @@ describe('Events', () => {
             truncate: true,
             cascade: true
         })
-                 
+
         Entity.create({
             id: 1,
             name: 'Test Entity',
             initials: 'TEST',
             description: 'test description'
-        }).then(function(entity) {
+
+        }).then(function (entity) {
             User.create({
                 id: 1,
                 username: 'TestUser',
                 name: 'Test User',
                 password: 'nasdasdasd',
                 email: 'email@email.com'
-            }).then(function(user) {
+
+            }).then(function (user) {
                 user.addEntity(entity);
-                EventModel.create({ 
+                EventModel.create({
                     title: "Test ",
-                    description:"Hello There",
+                    description: "Hello There",
                     start_date: "2018-10-13",
                     poster_id: 1,
                     entity_id: 1
-                }).then(function(event){
-                    event.addUser(user);                        
-                    done();
-                })           
-            })
-            .catch((err) => done());
-        })
-        .catch((err) => done())
-    });
 
-   /* describe('/POST Add Event', () => {
-        it('it should add a new event', (done) => {
-
-            let start_date = new Date();
-            let end_date = new Date();
-            start_date.setDate(start_date.getDate() + 1);
-            end_date.setDate(end_date.getDate() + 2);
-            let event = {
-                title: "Test Event",
-                description: "It is a test event, without content",
-                start_date: start_date,
-                end_date: end_date,
-                location: "Random Location",
-                price: 10,
-                poster_id: 1,
-                entity_id: 1
-            }
-
-            chai.request(app)
-                .post('/')
-                .send(event)
-                .end((err, res) => {
-                    res.should.have.status(201);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('title');
-                    res.body.should.have.property('description');
-                    res.body.should.have.property('start_date');
-                    res.body.should.have.property('end_date');
-                    res.body.should.have.property('location');
-                    res.body.should.have.property('price');
-                    res.body.should.have.property('entity_id');
+                }).then(function (event) {
+                    event.addUser(user);
                     done();
                 })
-                done();
-        })
-    });*/
+            }).catch((err) => done());
+        }).catch((err) => done());
+    });
 
 
     describe('/GET List Events App', () => {
         it('it should list all events on the App', (done) => {
 
             chai.request(app)
-            .get('/app')
-            .query({page: 0, limit: 10})
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                res.body.length.should.be.eql(1);
-                done();
-              })          
-            });
-        });   
+                .get('/app')
+                .query({ page: 0, limit: 10 })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(1);
+                    done();
+                })
+        });
+    });
 
-    
+
 });
